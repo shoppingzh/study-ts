e1f4411de8e801e8310281c742fe9c6b6590b7d4
shoppingzh <1049262057@qq.com>
2022-03-30 22:33:10 +0800

    extend

---
+++ quick-start/extend.ts
@@ -0,0 +1,29 @@
+namespace Extend {
+
+  class Animal {
+    private name: string;
+    constructor(name: string) {
+      this.name = name
+    }
+
+    sayHello(): void {
+      console.log(`My name is ${this.name}`)
+    }
+  }
+  
+  class Dog extends Animal {
+    constructor(name: string) {
+      super(name)
+    }
+
+    sayHello(): void {
+      console.log('I\'am a dog')
+      super.sayHello()
+    }
+  }
+
+  const dog = new Dog('Lili')
+  dog.sayHello()
+  // console.log(dog.name)
+  
+}
