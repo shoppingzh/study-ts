1b41ca95f938448fd11a5db8dc0e95538658bc9e
shoppingzh <1049262057@qq.com>
2022-04-17 11:01:00 +0800

    update type

--- quick-start/07 - type.ts
+++ quick-start/07 - type.ts
@@ -1,5 +1,13 @@
 namespace Type {
   type Mode = 'dark' | 'light' | 'auto'
+  interface Event {
+    type: string;
+    data: object;
+  }
+  type Callback = (event?: Event) => void;
 
   const mode: Mode = 'dark'
+  const callback: Callback = (event) => {
+    console.log(event.data)
+  }
 }
